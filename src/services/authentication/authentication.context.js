72508a4c552ec2975a160574580c0c031e64b3a6
Oscar Schenk <schenkoscar@gmail.com>
2022-07-12 15:32:33 +0200

    installed dotenv to avoid future mistakes

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,6 +1,6 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth();
 
@@ -33,17 +33,17 @@ export const AuthenticationContextProvider = ({ children }) => {
   //   return;
   // };
 
-  const [user, setUser] = useState(undefined);
+  const [users, setUsers] = useState(undefined);
 
   useEffect(() => {
     const unsubscribeFromAuthStatuChanged = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
-        setUser(user);
+        setUsers(user);
       } else {
         // User is signed out
-        setUser(undefined);
+        setUsers(undefined);
       }
     });
 
