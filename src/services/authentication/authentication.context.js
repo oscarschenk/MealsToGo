4b8c4dac24a9d5adce73d59a7fa552b211727139
Oscar Schenk <schenkoscar@gmail.com>
2022-07-12 15:53:49 +0200

    broke app with auth, api keys to env, will fix later

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -6,50 +6,9 @@ const auth = getAuth();
 
 export const AuthenticationContext = createContext();
 
+const useAuthentication = () => {};
+
 export const AuthenticationContextProvider = ({ children }) => {
-  // const [isLoading, setIsLoading] = useState(false);
-  // // const [user, setUser] = useState(null);
-  // const [error, setError] = useState(null);
-
-  // const [userLogged, setUserLogged] = useState(false);
-  // const [userProfile, setUserProfile] = useState(null);
-
-  // useEffect(() => {
-  //   Firebase.auth().onAuthStateChanged((user) => {
-  //     setUserLogged(user ? true : false);
-  //     setIsLoading(false);
-  //     setUserProfile(user);
-  //   });
-  // }, []); //<--- this means that useEffet will only run once, and not on every update
-
-  // const signOutUser = () => {
-  //   Firebase.auth().signOut();
-  // };
-
-  // const handleLogin = (email, password) => {
-  //   Firebase.auth()
-  //     .signInWithEmailAndPassword(email, password)
-  //     .catch((e) => setError(e.toString()));
-  //   return;
-  // };
-
-  const [users, setUsers] = useState(undefined);
-
-  useEffect(() => {
-    const unsubscribeFromAuthStatuChanged = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        setUsers(user);
-      } else {
-        // User is signed out
-        setUsers(undefined);
-      }
-    });
-
-    return unsubscribeFromAuthStatuChanged;
-  }, []);
-
   return (
     <AuthenticationContext.Provider value={{}}>
       {children}
